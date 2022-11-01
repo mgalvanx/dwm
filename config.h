fa18259f518e5ef188b8055d975ec6995c93dffe
mgalvanx <m.galvanx@gmail.com>
2022-10-31 22:13:11 -0500

    changed mpv newsboat title

--- config.h
+++ config.h
@@ -42,7 +42,8 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  iscentered  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         0,          1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          0,          -1,        -1 },
-	{ "mpv",     NULL,    "newboat-mpv",   0,         0,          1,          0,           0,        -1 },
+	{ "mpv",     NULL,    "newsboat-mpv",   0,         0,          1,          0,           0,        -1 },
+	{ "Soffice", NULL,     NULL,           0,         0,          0,          1,           0,        -1 },
 	{ "mpv",     NULL,    "ytfzf-mpv",     0,         0,          1,          0,           0,        -1 },
 	{ "st",      NULL,     NULL,           0,         0,          0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,          0,           1,        -1 }, /* xev */
