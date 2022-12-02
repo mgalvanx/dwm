606dfe0c319ff8ce71627bf33e27772c327ad1c2
mgalvanx <m.galvan@gmail.com>
2022-12-01 20:39:10 -0600

    changed some defaults

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static unsigned int snap      = 32;       /* snap pixel */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
@@ -32,7 +32,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -43,10 +43,9 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         0,          1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          0,          -1,        -1 },
 	{ "mpv",     NULL,    "newsboat-mpv",  0,         0,          1,          0,           0,        -1 },
+	{ "mpv",     NULL,    NULL,            0,         0,          1,          0,           0,        -1 },
 	{ "libreoffice", NULL,     NULL,       0,         0,          0,          0,           0,        -1 },
-	{ "mpv",     NULL,    "ytfzf-mpv",     0,         0,          1,          0,           0,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          0,          1,           0,        -1 },
-	{ "St",      NULL,     "worknote",     0,         1,          1,          0,           0,        -1 },
 	{ "XTerm",   NULL,     "todos",        0,         1,          1,          0,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,          0,           1,        -1 }, /* xev */
 };
@@ -180,7 +179,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_x,      incrgaps,	   {.i = -3 } },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} }, 
 	/* { MODKEY,			XK_v,		spawn,		SHCMD("") }, */
-  { MODKEY,       XK_n,      spawn,          SHCMD(TERMINAL " -e worknote") },
+  /*{ MODKEY,       XK_n,      spawn,          SHCMD(TERMINAL " -e worknote") },*/
   { MODKEY,       XK_m,      spawn,          SHCMD("xterm -e todos") },
 	/*{ MODKEY,			XK_n,		spawn,		SHCMD(TERMINAL " -e nvim -c VimwikiIndex") },
 	{ MODKEY|ShiftMask,		XK_n,		spawn,		SHCMD(TERMINAL " -e newsboat; pkill -RTMIN+6 dwmblocks") },
